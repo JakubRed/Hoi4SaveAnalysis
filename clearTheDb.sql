02b56615495c936710cde21b1b1e8126367ea76e
Jakub Redmerski <jakubredmerski@gmail.com>
2025-04-06 21:07:40 +0200

    add: miscellaneous files

---
+++ clearTheDb.sql
@@ -0,0 +1,8 @@
+DELETE FROM Construction;
+DELETE FROM Country;
+DELETE FROM Equipment_production;
+DELETE FROM Fuel;
+DELETE FROM States_backup;
+DELETE FROM States;
+DELETE FROM General_info;
+VACUUM;
