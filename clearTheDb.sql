93715ccfabf1beb05939e31325da15f49c1e759d
Jakub Redmerski <jakubredmerski@gmail.com>
2025-04-09 21:36:58 +0200

    update miscallenious files

--- clearTheDb.sql
+++ clearTheDb.sql
@@ -2,7 +2,6 @@ DELETE FROM Construction;
 DELETE FROM Country;
 DELETE FROM Equipment_production;
 DELETE FROM Fuel;
-DELETE FROM States_backup;
 DELETE FROM States;
+DELETE FROM Dataset_date;
 DELETE FROM General_info;
-VACUUM;
